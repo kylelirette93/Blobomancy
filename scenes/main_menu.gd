c250f498d197755a5f103442f447f4d27d50d321
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2025-08-06 18:49:10 -0300

    Created a few more scenes for menu selection
    Additionally created cool button hover effect.

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,14 +1,25 @@
 extends Control
 
+@export var tween_intensity: float
+@export var tween_duration: float
+
+@onready var battle: Button = $VBoxContainer/ButtonBattle
+@onready var options: Button = $VBoxContainer/ButtonOptions
+@onready var credits: Button = $VBoxContainer/ButtonCredits
+@onready var exit: Button = $VBoxContainer/ButtonExit
+
 func _ready():
 	pass
 	
 func _process(delta):
-	pass
+	btn_hovered(battle)
+	btn_hovered(options)
+	btn_hovered(credits)
+	btn_hovered(exit)
 
 
 func _on_button_battle_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/player.tscn")
+	get_tree().change_scene_to_file("res://scenes/ModeSelection.tscn")
 
 
 func _on_button_options_pressed() -> void:
@@ -18,6 +29,13 @@ func _on_button_options_pressed() -> void:
 func _on_button_credits_pressed() -> void:
 	pass # Replace with function body.
 
+func start_tween(object: Object, property: String, final_val: Variant, duration: float):
+	var tween = create_tween()
+	tween.tween_property(object, property, final_val, duration)
 
-func _on_button_exit_pressed() -> void:
-	get_tree().quit()
+func btn_hovered(button: Button):
+	button.pivot_offset = button.size / 2
+	if button.is_hovered():
+		start_tween(button, "scale", Vector2.ONE * tween_intensity, tween_duration)
+	else:
+		start_tween(button, "scale", Vector2.ONE, tween_duration)
---
+++ scenes/mode_selection.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_button_arcade_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_split_screen_pressed() -> void:
+	pass # Replace with function body.
